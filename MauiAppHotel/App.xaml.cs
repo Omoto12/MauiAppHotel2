fd97b5c84fc0e2746443291418283ef271afb398
GIOVANA OMOTO <giovana.omoto@gmail.com>
2026-05-19 21:50:37 -0300

    Add recursos ao app

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -13,8 +13,8 @@
         {
             var window = base.CreateWindow(activationState);
 
-            window.Width = 400;
-            window.Height = 600;
+            window.Width = 500;
+            window.Height = 700;
 
             return window;
         }
--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,10 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Cinzel-Black.ttf", "CinzelBlack");
+                    fonts.AddFont("Cinzel-Bold.ttf", "CinzelBold");
+                    fonts.AddFont("Cinzel-Regular.ttf", "CinzelRegular");
+                    fonts.AddFont("Cinzel-SemiBold.ttf", "CinzelSemiBold");
                 });
 
 #if DEBUG
