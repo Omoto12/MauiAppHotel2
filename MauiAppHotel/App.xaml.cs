3420d980424728960e9d1bf7ffd7f38bd9e6a956
GIOVANA OMOTO <giovana.omoto@gmail.com>
2026-05-20 21:20:41 -0300

    Modificações Calendário

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,7 +1,40 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo Estéticamente Raro só Existem 3 no Mundo",
+                ValorDiariaAdulto = 650.0,
+                ValorDiariaCrianca = 300.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte E o Caba Vai Endoidar é?",
+                ValorDiariaAdulto = 450.0,
+                ValorDiariaCrianca = 235.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Somente Eu",
+                ValorDiariaAdulto = 350.0,
+                ValorDiariaCrianca = 180.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Coitadinho da Silva",
+                ValorDiariaAdulto = 199.99,
+                ValorDiariaCrianca = 99.99
+            },
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,12 +2,31 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+    App PropriedadesApp;
+
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Today;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month + 1, DateTime.Today.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddYears(1);
+
 	}
     private async void BtnAvancar_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new SobreHotel());
-    }
+
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddYears(1);
 }
